b29d57629c77cf73f82a02fff73a2a6ff1432d84
thiago <thiago@thremes.com.br>
2012-11-03 12:50:59 -0200

    added new background image and minor enhancements on stylesheet

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ function adroa_theme_setup() {
     add_theme_support(
         'custom-background',
         array(
-            'default-image' => trailingslashit( get_template_directory_uri() ) . 'images/bg.jpg',
+            'default-image' => trailingslashit( get_template_directory_uri() ) . 'images/noisy_grid.png',
             'wp-head-callback' => 'adroa_custom_background_callback'
         )
     );
